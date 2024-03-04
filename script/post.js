0737a0005e49d7d782e9d66cb0c2b3697c33f93f
kaziabulhasib <abulhasibkazi@gmail.com>
2024-03-04 12:07:17 +0530

    latest post dynamic

--- script/post.js
+++ script/post.js
@@ -1,4 +1,4 @@
-const latestPost = async () => {
+const allPost = async () => {
   const response = await fetch(
     "https://openapi.programming-hero.com/api/retro-forum/posts"
   );
@@ -53,7 +53,7 @@ const latestPost = async () => {
   });
 };
 
-latestPost();
+allPost();
 
 let clickCount = 0;
 const sidebarParent = document.getElementById("sidebar-parent");
@@ -71,3 +71,56 @@ const clickPost = (e) => {
   sidebarParent.appendChild(div);
   console.log(e.target);
 };
+
+// latst post function -------------------------------------------
+
+const latestPost = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/retro-forum/latest-posts"
+  );
+
+  const data = await response.json();
+  // console.log(data[0].title);
+  const singlePostDiv = document.getElementById("single-post-div");
+
+  data.forEach((post) => {
+    // console.log(post.title);
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <!-- single container   -->
+          <div class="single-post-container">
+            <div class="single-post-img-div">
+              <img
+                src=" ${post.cover_image} "
+                alt=""
+                class="latest-post-thumb" />
+            </div>
+            <!-- date container  -->
+            <div class="date-container">
+              <i class="fa-regular fa-calendar-plus"></i>
+              <p>${post.author.posted_date}</p>
+            </div>
+            <h4>
+             ${post.title}
+            </h4>
+            <p id="single-post-description">
+            ${post.description}
+            </p>
+            <!-- author descriptin div  -->
+            <div class="autthor-des-div">
+              <img src="${post.profile_image}" alt="" />
+              <div>
+                <h5 class="auth-name">${post.author.name}</h5>
+                <p class="auth-desig">${post.author.designation}</p>
+              </div>
+            </div>
+          </div>
+          <!-- single container  end  -->
+
+    `;
+    // console.log(data[0].title);
+    singlePostDiv.appendChild(div);
+  });
+};
+
+latestPost();
